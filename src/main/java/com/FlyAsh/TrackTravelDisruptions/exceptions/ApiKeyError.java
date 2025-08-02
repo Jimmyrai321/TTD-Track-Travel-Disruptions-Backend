42df753bdbcba8315d0b64fe108e16e436609c1e
jimmyrai321 <jimmyrai321@gmail.com>
2025-08-02 18:44:32 +0100

    git.ignore update + apikey now in res

---
+++ src/main/java/com/FlyAsh/TrackTravelDisruptions/exceptions/ApiKeyError.java
@@ -0,0 +1,7 @@
+package com.FlyAsh.TrackTravelDisruptions.exceptions;
+
+public class ApiKeyError extends RuntimeException {
+    public ApiKeyError(String message) {
+        super(message);
+    }
+}
